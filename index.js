973e72b5481d7a4c3b644b96113b1f08c684ef76
Showkat  Ali <uniqueshowkat114724@gmail.comi>
2022-08-28 00:18:38 +0600

    server-created-for-WEBDOCTO

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT || 5000;
+require("dotenv").config();
+
+app.use(cors());
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
