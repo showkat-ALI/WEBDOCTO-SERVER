e3467bac22cd3531dbe1fa3bff8436ca9177d959
Showkat  Ali <uniqueshowkat114724@gmail.comi>
2022-09-01 02:04:54 +0600

    mongodb-connected

--- index.js
+++ index.js
@@ -2,9 +2,24 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
-require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
+require("dotenv").config();
 app.use(cors());
+// Database connection
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.goxkt.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
+//New Client
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+async function run() {
+  await client.connect();
+  console.log("mongobd connected");
+}
+run().catch(console.dir);
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
